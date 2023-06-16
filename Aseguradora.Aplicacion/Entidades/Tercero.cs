4caa514331a931b509f90526cee2b0c2258be01d
Nickylyl <131604162+Nickylyl@users.noreply.github.com>
2023-06-15 21:58:51 -0300

    Update Tercero.cs

--- Aseguradora.Aplicacion/Entidades/Tercero.cs
+++ Aseguradora.Aplicacion/Entidades/Tercero.cs
@@ -2,7 +2,7 @@ namespace Aseguradora.Aplicacion;
 
 public class Tercero : Persona
 {
-    public string? Aseguradora { get; set; } = "";
+    public string? Aseguradora { get; set; } 
     public int Siniestro { get; set; }
 
     public Tercero(int dni, string apellido, string nombre, string aseguradora, int siniestro):base(dni, apellido, nombre)
